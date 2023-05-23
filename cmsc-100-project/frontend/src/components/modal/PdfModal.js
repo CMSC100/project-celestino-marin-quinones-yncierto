277cc8848dbcfc82088da500e28f2a0c5790e4b7
Sean Joseph Marin <scmarin@up.edu.ph>
2023-05-23 11:05:56 +0800

    feat: added style to PDF content

--- cmsc-100-project/frontend/src/components/modal/PdfModal.js
+++ cmsc-100-project/frontend/src/components/modal/PdfModal.js
@@ -1,40 +1,78 @@
 import React from "react";
-import { Page, Text, View, Document, StyleSheet, PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet, PDFViewer, PDFDownloadLink, Line, Svg, Font} from '@react-pdf/renderer';
+import { printPlugin } from '@react-pdf-viewer/print';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import { Button, Spinner, Viewer } from '@react-pdf-viewer/core'
+import '@react-pdf-viewer/print/lib/styles/index.css';
 import './Modal.css'
 
 export default function PdfModal ({ setpdfModal }) {
+  let fName = "Juan"
+  let mName = "Santos"
+  let lName = "Dela Cruz"
   let applicationDetails = {
-    studName: "Sean Joseph C. Marin",
+    studName: `${fName} ${mName} ${lName}`,
     studNo: "2021-07177",
     acadAdviserName: "Reginald Recario",
     clearanceOfficer: "Katherine Tan",
     currDate: `${new Date().toLocaleDateString()}`
   }
 
+  const customProgressBar = (numLoadedPages, numPages) => (
+    <div>
+      <div>
+        Preparing pages...
+      </div>
+      <div>
+        <Spinner />
+      </div>
+      <Button onClick={() => {}}>Cancel</Button>
+    </div>
+  )
+
+  // const printPluginInstance = printPlugin()
+  // const { PrintButton } = printPluginInstance
+
   return (
     <div className="pdfmodalBackground">
       <div className="pdfmodalContainer">
-        <PDFViewer className="pdf-file" showToolbar={false}>
-          <PDFReactPDF applicationDetails={applicationDetails}/>
-        </PDFViewer>
+        {pdfViewer(applicationDetails)}
+        {/* <PrintButton />
+        <Viewer fileUrl="./sample.pdf" plugins={[printPluginInstance]}/> */}
         <div className="footer">
           <button onClick={() => { setpdfModal(false); }} id="cancelBtn"> 
             Cancel 
           </button>
-          <PDFDownloadLink document={<PDFReactPDF applicationDetails={applicationDetails}/>} fileName="example.pdf">
-              {({ blob, url, loading, error }) =>
-                loading
-                  ? "" 
-                  : <button type="button">Button</button>
-              }
-            </PDFDownloadLink>
+          {pdfDownload(applicationDetails, lName)}
+          {/* <button type="button" onClick={
+            print
+          }>Print</button> */}
         </div>
       </div>
     </div>
   )
 }
 
+function pdfViewer(applicationDetails) {
+  return (
+    <PDFViewer className="pdf-file">
+      <PDFReactPDF applicationDetails={applicationDetails}/>
+    </PDFViewer>
+  )
+}
+
+function pdfDownload(applicationDetails, lName) {
+  <PDFDownloadLink document={<PDFReactPDF applicationDetails={applicationDetails}/>} fileName={`ApplicationClearance_${lName}.pdf`}>
+    {({ blob, url, loading, error }) =>
+      loading
+        ? "" 
+        : <button type="button">Download</button>
+    }
+  </PDFDownloadLink>
+}
+
 function PDFReactPDF(props) {
+  Font.register({family: "Times-Roman", src: "source"})
   let { studName, studNo, acadAdviserName, clearanceOfficer, currDate } = props.applicationDetails
   const styles = StyleSheet.create({
     document: {
@@ -42,50 +80,74 @@ function PDFReactPDF(props) {
       alignContent: "flex-start",
       justifyContent: "center",
       alignItems: "flex-start",
+      fontFamily: "Times-Roman"
     },
     page: {
       flexDirection: 'column',
-      backgroundColor: '#E4E4E4',
-      alignItems: "center",
+      paddingTop: "1in",
+      paddingBottom: "1in",
+      paddingLeft: "1in",
+      paddingRight: "1in",
+      width: "100%",
+      rowGap: 30
     },
-    section: {
-      margin: 10,
-      padding: 10,
-      flexGrow: 1
+    header: {
+     flexDirection: "column",
+     alignItems: "center",
+     rowGap: 10
     },
     title: {
-      fontSize: 22
+      fontSize: 24
     },
     subtitle: {
-      fontSize: 16
+      fontSize: 18
     },
     text: {
-      fontSize: 14
+      fontSize: 16
     },
+    horizontalLine: {
+      borderWidth: 0.5,
+      borderColor: "black",
+      margin: 10
+    }
   });
 
   return(
     <Document style={styles.document}>
       <Page size="A4" style={styles.page}>
-        <View>
-          <Text style={styles.title}>University of the Philippines</Text>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.title}>University of the Philippines</Text>
+          </View>
+          <View style={{alignItems: "center", rowGap: 5}}>
+            <Text style={styles.subtitle}>College of Arts and Sciences</Text>
+            <Text style={styles.subtitle}>Institute of Computer Science</Text>
+          </View>
         </View>
-        <View>
-          <Text style={styles.subtitle}>College of Arts and Sciences</Text>
-          <Text style={styles.subtitle}>Institute of Computer Science</Text>
-        </View>
+        <Svg height="1" width="100%">
+          <Line
+            x1="0"
+            y1="0"
+            x2="500"
+            y2="0"
+            strokeWidth={1}
+            stroke="black"
+          />
+        </Svg>
         <View>
           <Text style={styles.text}>{currDate}</Text>
         </View>
         <View>
-          <Text style={styles.text}>This document certifies that {studName}, {studNo} has satisfied the clearance requirements of the institute.</Text>
+          <Text style={styles.text}>This document certifies that {studName}, {studNo}, has satisfied the clearance requirements of the institute.</Text>
         </View>
-        <View>
-          <Text style={styles.text}>Verified:</Text>
-        </View>
-        <View>
-          <Text style={styles.text}>Academic Adviser: {acadAdviserName}</Text>
-          <Text style={styles.text}>Clearance Officer: {clearanceOfficer}</Text>
+        <View style={{rowGap: 15}}>
+          <View>
+            <Text style={styles.text}>Verified:</Text>
+          </View>
+          <View style={{rowGap: 5}}>
+            <Text style={styles.text}>Academic Adviser: {acadAdviserName}</Text>
+            <Text style={styles.text}>Clearance Officer: {clearanceOfficer}</Text>
+          </View>
         </View>
       </Page>
     </Document>
