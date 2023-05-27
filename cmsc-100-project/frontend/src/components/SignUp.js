29207d9233ef42da1ce178385d6be9f2c8fafa58
kyla <kmcelestino@up.edu.ph>
2023-05-27 13:22:26 +0800

    Refactor: Changed the position of navigation bar

--- cmsc-100-project/frontend/src/components/SignUp.js
+++ cmsc-100-project/frontend/src/components/SignUp.js
@@ -100,7 +100,7 @@ export default function SignUp() {
   return (
     <>
       <div className="signup-container">
-        <form onSubmit={handleSignUp} onReset={handleReset}>
+        <form onSubmit={handleSignUp} onReset={handleReset} className="signup-form">
           <div className="imgcontainer">
             <img src="https://hips.hearstapps.com/hmg-prod/images/domestic-cat-lies-in-a-basket-with-a-knitted-royalty-free-image-1592337336.jpg?crop=0.668xw:1.00xh;0.247xw,0&resize=1200:*" alt="Avatar" className="avatar" />
           </div>
