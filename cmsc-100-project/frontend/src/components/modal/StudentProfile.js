d31fec2f9784307683a65d9e1129cf5f04db3eb6
kyla <kmcelestino@up.edu.ph>
2023-05-27 13:14:03 +0800

    Feat: Added students' profile modal

---
+++ cmsc-100-project/frontend/src/components/modal/StudentProfile.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Modal.css'
+
+export default function StudentProfile ({setProfileModal}) {
+
+  return(
+    <div className="profile-bg">
+      <div className="profile-container">
+        <div className='image-text'>
+          <span className='image'>
+              <img src= {require("./aprub.png")} alt=""/>
+          </span>
+
+          <div className='text header-text'>
+              {/* dapat dito yung fullname ng nag log in*/}
+              <span className='name'>Kyla Cristine Miraflor Celestino</span>
+              {/* dito yung type ng user */}
+              <span className='usertype'>Student</span>
+          </div>
+        </div>
+        <div>
+          {/* student number, email, adviser, application id(?) */}
+        </div>
+        <button onClick={() => { setProfileModal(false); }} id="cancelBtn"> Close </button>
+      </div>
+
+    </div>
+  )
+}
