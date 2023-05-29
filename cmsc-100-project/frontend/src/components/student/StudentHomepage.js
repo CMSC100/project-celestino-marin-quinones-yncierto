841eb5e820a4f8145343eb25392702aeb8eee1a6
Reymond <rpyncierto@up.edu.ph>
2023-05-29 16:39:47 +0800

    chore: aligned the application number to its supposedly number

--- cmsc-100-project/frontend/src/components/student/StudentHomepage.js
+++ cmsc-100-project/frontend/src/components/student/StudentHomepage.js
@@ -136,7 +136,7 @@ export default function StudentHomepage() {
             key={index}
           >
             <div className='application-info'>
-              <h4>Application {index + 1}</h4>
+              <h4>Application {applications.length - index}</h4>
               {application.status === 'open' && application.studentSubmission.length === 0 ? (
                 <>
                   <p><b>Name:</b> {userData.fullName}</p>
