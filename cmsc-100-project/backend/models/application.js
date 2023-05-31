7461d68ff5392eb1a158e72e7a00aa9a859647f2
Sean Joseph Marin <scmarin@up.edu.ph>
2023-05-31 17:21:11 +0800

    style: change application schema

--- cmsc-100-project/backend/models/application.js
+++ cmsc-100-project/backend/models/application.js
@@ -3,21 +3,14 @@ import mongoose from "mongoose";
 const ApplicationSchema = new mongoose.Schema({
     status: { type: String, required: true },
     step: {type: Number, required: true},
-    remarks: [],
-    // remarks: [{
-    //     remark: {type: String, required: true},
-    //     date: {type: Date, default: Date.now},
-    //     commenter: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required : true},
-    // }],
-    // studentSubmission: [],
+    remarks: [{
+        remark: {type: String, required: true},
+        date: {type: Date, default: Date.now},
+        commenter: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required : true},
+    }],
     studentSubmission: [{
         githubLink: {type: String},
         createdAt: { type: Date, default: Date.now },
-        remarks: [{
-            remark: {type: String, required: true},
-            date: {type: Date, default: Date.now},
-            stepGiven: {type: Number}
-        }]
     }],
     studentID: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required : true},
     adviserID: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required : true},
